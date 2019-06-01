196b1027b80485c308ccd6766981e93e999cd860
Suhaila <40865556+suhailaben@users.noreply.github.com>
2019-05-31 17:53:04 -0700

    service and transformation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '7336a31d1757994fae2e39aa30117fcf',
+  baseUrl: 'http://',
 };
 
 /*
